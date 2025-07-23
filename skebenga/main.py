8366fe9b9e20836c25044d01ea92dc2d868a0bfa
vrtx <vortexshrimp@gmail.com>
2025-07-23 19:01:12 +0200

    Raise exceptions in `main()` for potential errors.

--- skebenga/main.py
+++ skebenga/main.py
@@ -21,8 +21,8 @@ class ClashBot(commands.Bot):
         await self.setup_coc_api()
 
     async def on_ready(self) -> None:
-        print(f'Logged in as {self.user}.')
-        activity = discord.Activity(type=discord.ActivityType.watching, name='Clash of Clans')
+        print(f'[info] Logged in as {self.user}.')
+        activity: discord.Activity = discord.Activity(type=discord.ActivityType.watching, name='Clash of Clans')
         await self.change_presence(activity=activity)
 
     # Load any cogs found in the cogs directory.
@@ -31,21 +31,17 @@ class ClashBot(commands.Bot):
             if file.endswith('.py'):
                 try:
                     await self.load_extension(f'cogs.{file[:-3]}')
-                    print(f'Loaded {file} extension')
+                    print(f'[info] Loaded {file} extension.')
                 except Exception as error:
-                    print(f'[error] Failed to load extension {file}. Error {error}')
+                    print(f'[error] Failed to load extension {file}. Error {error}.')
 
     async def setup_coc_api(self) -> None:
-        # Example discord bot to follow.
-        # https://github.com/mathsman5133/coc.py/blob/master/examples/discord_bot_with_cogs.py
-        # https://peps.python.org/pep-0008/
-
         self.coc_client.add_clan_updates(self.coc_clantag)
         self.coc_client.add_war_updates(self.coc_clantag)
 
         try:
             clan: coc.Clan = await self.coc_client.get_clan(self.coc_clantag)
-            print(f'Tracking {clan.name} with tag {clan.tag}')
+            print(f'[info] Tracking {clan.name}{clan.tag} clan.')
             self.coc_client.add_player_updates(*[member.tag for member in clan.members])
         except coc.ClashOfClansException:
             print(f'[error] Failed to start tracking {self.coc_clantag}')
@@ -70,42 +66,35 @@ class ClashBot(commands.Bot):
 async def main() -> None:
     discord_token: str | None = os.getenv('DISCORD_TOKEN')
     if discord_token is None:
-        print('[error] DISCORD_TOKEN not found in .env file.')
-        return
+        raise ValueError('[error] DISCORD_TOKEN not found in .env file.')
 
     coc_email: str | None = os.getenv('COC_EMAIL')
     if coc_email is None:
-        print('[error] COC_EMAIL not found in .env file.')
-        return
-    
+        raise ValueError('[error] COC_EMAIL not found in .env file.')
+
     coc_password: str | None = os.getenv('COC_PASSWORD')
     if coc_password is None:
-        print('[error] COC_PASSWORD not found in .env file.')
-        return
+        raise ValueError('[error] COC_PASSWORD not found in .env file.')
 
+    # Make sure the clan tag is provided and valid.
     bot_globals.COC_CLANTAG = os.getenv('COC_CLAN_TAG')
     if bot_globals.COC_CLANTAG is None:
-        print('[error] COC_CLAN_TAG not found in .env file.')
-        return
+        raise ValueError('[error] COC_CLAN_TAG not found in .env file.')
 
     if not coc.utils.is_valid_tag(bot_globals.COC_CLANTAG):
-        print(f'[error] Invalid clan tag format provided: {bot_globals.COC_CLANTAG}')
-        return
+        raise ValueError(f'[error] Invalid clan tag format provided: {bot_globals.COC_CLANTAG}')
 
     bot_globals.DISCORD_CLAN_WEBHOOK = os.getenv('DISCORD_CLAN_WEBHOOK')
     if bot_globals.DISCORD_CLAN_WEBHOOK is None:
-        print('[error] DISCORD_CLAN_WEBHOOK not found in .env file. Clan events will not be sent to Discord.')
-        return
+        raise ValueError('[error] DISCORD_CLAN_WEBHOOK not found in .env file. Clan events will not be sent to Discord.')
 
     bot_globals.DISCORD_WAR_WEBHOOK = os.getenv('DISCORD_WAR_WEBHOOK')
     if bot_globals.DISCORD_WAR_WEBHOOK is None:
-        print('[error] DISCORD_WAR_WEBHOOK not found in .env file. War events will not be sent to Discord.')
-        return
+        raise ValueError('[error] DISCORD_WAR_WEBHOOK not found in .env file. War events will not be sent to Discord.')
 
     bot_globals.DISCORD_DONATIONS_WEBHOOK = os.getenv('DISCORD_DONATIONS_WEBHOOK')
     if bot_globals.DISCORD_DONATIONS_WEBHOOK is None:
-        print('[error] DISCORD_DONATIONS_WEBHOOK not found in .env file. Donations will not be sent to Discord.')
-        return
+        raise ValueError('[error] DISCORD_DONATIONS_WEBHOOK not found in .env file. Donations will not be sent to Discord.')
 
     async with coc.EventsClient() as coc_client:
         # Attempt to log into the CoC API.
@@ -128,12 +117,13 @@ if __name__ == '__main__':
         try:
             loop.run_until_complete(main())
             loop.run_forever()
-        except KeyboardInterrupt:
-            ...
+        except (KeyboardInterrupt, ValueError) as error:
+            print(error)
     # If the .env file does not exist, create one.
     else:
-        print('[error] Failed to load ".env" file. Creating one for you.')
+        print('[error] Failed to load ".env" file. Creating one for you. Please fill it out before running the bot again.')
 
+        # Create a new .env file with the required fields.
         with open('.env', 'w') as env_file:
             env_file.write(
                 '# Standard Discord bot application token.\n'
@@ -151,5 +141,3 @@ if __name__ == '__main__':
                 '# The tag of the clan to track.\n'
                 'COC_CLAN_TAG=\n'
             )
-
-        print('[error] Please fill in the .env file with your data and try again.')
